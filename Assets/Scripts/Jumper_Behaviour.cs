9d3984c8580b0b7069c5810fd26bb736832f80e5
Rousselot Mathis <mathisr2207@hotmail.fr>
2019-04-30 18:44:27 +0200

    Update special bloc

---
+++ Assets/Scripts/Jumper_Behaviour.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper_Behaviour : MonoBehaviour
+{
+    public int forceX = 30;
+    public int forceY = 30;
+}
