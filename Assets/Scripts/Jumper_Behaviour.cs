307fa26c0a73c86ec42520f2697357bd56b573b9
Rousselot Mathis <mathisr2207@hotmail.fr>
2019-04-30 18:44:49 +0200

    Revert "Update special bloc"
    This reverts commit 9d3984c8580b0b7069c5810fd26bb736832f80e5.

--- Assets/Scripts/Jumper_Behaviour.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Jumper_Behaviour : MonoBehaviour
-{
-    public int forceX = 30;
-    public int forceY = 30;
-}
