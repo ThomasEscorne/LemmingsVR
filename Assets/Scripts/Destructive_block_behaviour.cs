1172c4ef0357811e8ae77c2be2cfc556748ad707
Rousselot Mathis <mathisr2207@hotmail.fr>
2019-05-01 03:54:22 +0200

    Revert "Revert "Update special bloc""
    This reverts commit 307fa26c0a73c86ec42520f2697357bd56b573b9.

---
+++ Assets/Scripts/Destructive_block_behaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Destructive_block_behaviour : MonoBehaviour
+{
+    public int popCollapse = 4;
+    private bool hasCollapse = false;
+    private TextMeshPro infoText;
+
+    private void Start()
+    {
+        infoText = transform.GetChild(0).GetComponent<TextMeshPro>();
+        infoText.text = "x" + popCollapse;
+    }
+
+    public void addPerson()
+    { popCollapse++; infoText.text = "x" + popCollapse; }
+    public void substractPerson()
+    {
+        popCollapse--;
+        if (!hasCollapse && popCollapse <= 0)
+        {
+            hasCollapse = true;
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
+            infoText.text = "";
+        }
+        else if (!hasCollapse)
+        {
+            infoText.text = "x" + popCollapse;
+        }
+    }
+}
