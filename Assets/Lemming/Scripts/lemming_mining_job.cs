29087d64031ff4ce9c40a2169edf0ed9968fd12a
samuelOsborne <samu.osborne@gmail.com>
2019-06-11 20:58:47 +0900

    Prepared main scene

--- Assets/Lemming/Scripts/lemming_mining_job.cs
+++ Assets/Lemming/Scripts/lemming_mining_job.cs
@@ -4,16 +4,18 @@ public class lemming_mining_job : MonoBehaviour
 {
     public string[] tagToCheck;
 
-    private int _rotation = 1;
+    private float _rotation = 0.5f;
+
+    private Vector3 positionToMine;
     
     public void DestroyMapRadius()
     {
-        var positionToMine = transform.position;
-        var length = new Vector3(1f, 0.1f, 0f);
+        positionToMine = gameObject.transform.position;
+        var length = new Vector3(1f, 0.2f, 0f);
 
         if (transform.eulerAngles.y >= 180)
-            _rotation = -1;
-        positionToMine.x *= _rotation;
+            _rotation = -0.5f;
+        positionToMine.x += _rotation;
         positionToMine.y += 0.25f;
         
         var objectsInRange = Physics.OverlapBox(positionToMine, length);
