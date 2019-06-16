43df100a4fcce2d9afa632e9a019a6537bd7b5a8
Rousselot Mathis <mathisr2207@hotmail.fr>
2019-06-16 07:27:44 +0200

    Update lemming_spawner_behaviour.cs

--- Assets/Lemming/Scripts/lemming_spawner_behaviour.cs
+++ Assets/Lemming/Scripts/lemming_spawner_behaviour.cs
@@ -27,7 +27,7 @@ public class lemming_spawner_behaviour : MonoBehaviour
 
     IEnumerator Spawn()
     {
-        for (int i = 0; i < number_of_lemmings; i++)
+        while (number_of_lemmings != 0)
         {
             GameObject container = Instantiate(lemming, new Vector3(transform.position.x, transform.position.y - 0.14f, transform.position.z), Quaternion.identity) as GameObject;
             container.transform.GetChild(0).gameObject.GetComponent<lemming_behavior>().Tilemap = TilemapForLemming;
@@ -41,7 +41,7 @@ public class lemming_spawner_behaviour : MonoBehaviour
                 GameObject lemming = container.transform.GetChild(0).gameObject;
                 lemming.GetComponent<lemming_behavior>().give_object(stuff.GetComponent<LemmingObject>());
             }
-           
+            number_of_lemmings--;
             yield return new WaitForSeconds(1.5f);
         }
         yield return new WaitForSeconds(2);
